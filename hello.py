4326391f142286484eab7fc01549e8dcc3df10f3
Petros Perlepes <petros@growth.gr>
2016-07-01 00:43:29 +0300

    Adding SQLAlchemy and first model classes

--- hello.py
+++ hello.py
@@ -1,19 +1,78 @@
-from flask import Flask, request, make_response, redirect, url_for, render_template
-from flask_script import Manager
+import os
+from flask import Flask, request, make_response, redirect, url_for, render_template, session, flash
+from flask_script import Manager, Server, Shell
+from flask_moment import Moment
 from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+#ADD TO GIT, TEST ASSETS AND TIME, COMPLETE FORMS AND EXPERIMENT WITH ALL
+
+
 app = Flask('flask-web')
+app.config.from_pyfile('config.cfg') #make from object
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///'+os.path.join(basedir,'data.sqlite')
 manager = Manager(app)
+moment = Moment(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
-@app.route('/')
+def make_shell_context():
+	return dict(app = app, db = db, Role = Role)
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer,primary_key = True)
+	name = db.Column(db.String(64), unique = True, index = True)
+	users = db.relationship('User', backref = 'role' , lazy = 'dynamic')
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(64), unique = True, index = True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+class NameForm(Form):
+	name = StringField('name', validators = [DataRequired()])
+	submit = SubmitField('Submit')		
+
+@app.route('/', methods = ['GET','POST'])
 def index():
-	return render_template('index.html')
-
-@app.route('/user/<name>')
-def user(name):
-	if(request.headers.get('User-Agent') != 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'):
+	import pdb;pdb.set_trace()
+	form = NameForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username = form.name.data).first()
+		if not user:
+			user = User(username = form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
+		session['name'] = form.name.data
+		form.name.data = ''
 		return redirect(url_for('index'))
-	return render_template('user.html', name = name)
+	return render_template('index.html', form = form, name = session.get('name'), known = session.get('known', False) )
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
+
+
+manager.add_command('debug', Server(host='0.0.0.0',port =5000, use_debugger = True, use_reloader = True))
+manager.add_command('shell', Shell(make_context = make_shell_context))
 if __name__ == '__main__':
+	db.create_all()
 	manager.run()
--- test.py
+++ test.py
@@ -1,22 +1,25 @@
 import unittest
+from flask_testing import TestCase
 import hello
 
-class HelloTestCase(unittest.TestCase):
+class HelloTestCase(TestCase):
+
+	def create_app(self):
+		app = hello.app
+		app.testing = True
+		return app
+
 	def setUp(self):
 		hello.app.testing = True
 		self.app = hello.app.test_client()
 
-	def test_home(self):
-		rv = self.app.get('/')
-		assert b'Hello World' in rv.data
 	
-	def test_user_agent_restriction(self):
-		rv = self.app.get('/user/Dave', follow_redirects = True)
-		self.assertIn(b'Hello World', rv.data)
 
-	def test_user_agent_allowance(self):
-		rv = self.app.get('/user/Dave',headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'} ,follow_redirects = True)
-		self.assertIn(b'Hello, Dave', rv.data)
+	def test_404_page(self):
+		rv = self.app.get('/Dave')
+		self.assert404(rv)
+		self.assertIn('Flasky - 404', rv.data)
+
 
 
 if __name__=='__main__':
