a5616805880fae1c2d40dcba1040a373d329a281
Petros Perlepes <petros@growth.gr>
2016-07-02 17:58:49 +0300

    form fix

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 class NameForm(Form):
-	name = StringField('What is your name?', validators = Required)
+	name = StringField('What is your name?', validators = [Required()])
 	submit = SubmitField('Submit')
